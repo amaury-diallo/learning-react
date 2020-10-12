b75d7eb528f438bab2c81c80b5af68eea381db21
amaury-diallo <amadoury313@gmail.com>
2020-10-12 20:44:34 +0000

    Basic routing added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,30 @@
 import React from "react";
+import { NavLink, Link } from "react-router-dom";
 
 const NavBar = (props) => (
-  <nav className="navbar navbar-dark bg-dark">
-    <a className="navbar-brand" href="#">
-      Navbar &nbsp;
-      <span className="badge badge-pill badge-secondary">
-        {props.counters.filter((c) => c.value > 0).length}
-      </span>
-    </a>
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <Link to="/" className="navbar-brand">
+      Vidly
+    </Link>
+    <div className="">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <NavLink to="/movies" className="nav-link">
+            Movies
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/customers" className="nav-link">
+            Customers
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/rentals" className="nav-link">
+            Rentals
+          </NavLink>
+        </li>
+      </ul>
+    </div>
   </nav>
 );
 
--- src/index.js
+++ src/index.js
@@ -8,11 +8,29 @@ import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 // import Counter from "./components/counter";
 import MovieList from "./components/MovieList";
+import NavBar from "./components/NavBar";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import Customers from "./pages/Customers";
+import Rentals from "./pages/Rentals";
+
+const VidlyApp = () => {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route path='/movies' component={MovieList}/>
+        <Route path="/customers" component={Customers}/>
+        <Route path="/rentals" component={Rentals}/>
+        <Redirect from="/" to="/movies"/>
+      </Switch>
+    </BrowserRouter>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
-    <MovieList />
+    <VidlyApp />   
   </React.StrictMode>,
   document.getElementById("root")
 );
