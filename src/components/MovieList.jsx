5171838f4117409caacc7bfc5dca94db1cf1fe15
amaury-diallo <amadoury313@gmail.com>
2020-10-12 18:15:28 +0000

    Filtering movies by genres

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,6 +6,7 @@ import { FaSortDown, FaSortUp } from "react-icons/fa";
 import { getMovies } from "../services/fakeMovieService";
 import Like from "./Like";
 import Pagination from "./Pagination";
+import { getGenres } from "../services/fakeGenreService";
 
 class MovieList extends Component {
   state = {
@@ -14,6 +15,8 @@ class MovieList extends Component {
     CURRENT_PAGE: 1,
     sortingOrder: "asc",
     sortedBy: null,
+    genres: [{ name: "All genres" }, ...getGenres()],
+    filterByGenre: null,
   };
 
   handleLike = ({ _id }) => {
@@ -54,80 +57,123 @@ class MovieList extends Component {
     ) : null;
   };
 
+  filterByGenre = (genre) => {
+    this.setState({
+      filterByGenre: genre.hasOwnProperty("_id") ? genre.name : null,
+      CURRENT_PAGE: 1,
+    });
+  };
+
+  getVisibleMovies = () => {
+    let { movies } = this.state;
+    movies = this.state.filterByGenre
+      ? movies.filter((movie) => movie.genre.name === this.state.filterByGenre)
+      : movies;
+    return movies;
+  };
+
+  toggleActiveFilter = (genre) => {
+    return genre.name === this.state.filterByGenre ||
+      (this.state.filterByGenre == null && !genre._id)
+      ? "active"
+      : "";
+  };
+
   renderMovies = () => {
     if (this.state.movies.length === 0)
       return <p>There are no movies to display</p>;
 
     return (
-      <main className="container pt-3">
-        <p>
-          Showing {this.state.movies.slice(...this.getRange()).length} movies in
-          the database
-        </p>
-        <table className="table">
-          <thead>
-            <tr>
-              <th
-                className="clickable"
-                onClick={() => this.sortMoviesBy("title")}
-              >
-                Title {this.toggleOrderingIcon("title")}
-              </th>
-              <th
-                className="clickable"
-                onClick={() => this.sortMoviesBy("genre.name")}
-              >
-                Genre {this.toggleOrderingIcon("genre.name")}
-              </th>
-              <th
-                className="clickable"
-                onClick={() => this.sortMoviesBy("numberInStock")}
-              >
-                Stock {this.toggleOrderingIcon("numberInStock")}
-              </th>
-              <th
-                className="clickable"
-                onClick={() => this.sortMoviesBy("dailyRentalRate")}
-              >
-                Rate {this.toggleOrderingIcon("dailyRentalRate")}
-              </th>
-              <th></th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.movies.slice(...this.getRange()).map((movie) => (
-              <tr key={movie._id}>
-                <td>{movie.title}</td>
-                <td>{movie.genre.name}</td>
-                <td>{movie.numberInStock}</td>
-                <td>{movie.dailyRentalRate}</td>
-                <th>
-                  <Like
-                    isLiked={movie.liked}
-                    onLike={this.handleLike}
-                    target={movie}
-                  />
-                </th>
-                <th>
-                  <button
-                    className="btn btn-danger btn-sm"
-                    onClick={this.handleDelete.bind(this, movie._id)}
+      <main className="container pt-5">
+        <div className="row">
+          <div className="col-3">
+            <ul className="list-group">
+              {this.state.genres.map((genre, index) => (
+                <li
+                  key={index}
+                  className={
+                    "list-group-item clickable " +
+                    this.toggleActiveFilter(genre)
+                  }
+                  onClick={() => this.filterByGenre(genre)}
+                >
+                  {genre.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="col">
+            <p>
+              Showing {this.getVisibleMovies().length} movies in the database
+            </p>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th
+                    className="clickable"
+                    onClick={() => this.sortMoviesBy("title")}
                   >
-                    Delete
-                  </button>
-                </th>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <Pagination
-          totalPage={Math.ceil(
-            this.state.movies.length / this.state.MOVIE_PER_PAGE
-          )}
-          gotoPage={this.gotoPage}
-          currentPage={this.state.CURRENT_PAGE}
-        />
+                    Title {this.toggleOrderingIcon("title")}
+                  </th>
+                  <th
+                    className="clickable"
+                    onClick={() => this.sortMoviesBy("genre.name")}
+                  >
+                    Genre {this.toggleOrderingIcon("genre.name")}
+                  </th>
+                  <th
+                    className="clickable"
+                    onClick={() => this.sortMoviesBy("numberInStock")}
+                  >
+                    Stock {this.toggleOrderingIcon("numberInStock")}
+                  </th>
+                  <th
+                    className="clickable"
+                    onClick={() => this.sortMoviesBy("dailyRentalRate")}
+                  >
+                    Rate {this.toggleOrderingIcon("dailyRentalRate")}
+                  </th>
+                  <th></th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.getVisibleMovies()
+                  .slice(...this.getRange())
+                  .map((movie) => (
+                    <tr key={movie._id}>
+                      <td>{movie.title}</td>
+                      <td>{movie.genre.name}</td>
+                      <td>{movie.numberInStock}</td>
+                      <td>{movie.dailyRentalRate}</td>
+                      <th>
+                        <Like
+                          isLiked={movie.liked}
+                          onLike={this.handleLike}
+                          target={movie}
+                        />
+                      </th>
+                      <th>
+                        <button
+                          className="btn btn-danger btn-sm"
+                          onClick={this.handleDelete.bind(this, movie._id)}
+                        >
+                          Delete
+                        </button>
+                      </th>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+            <Pagination
+              totalPage={Math.ceil(
+                this.getVisibleMovies().length / this.state.MOVIE_PER_PAGE
+              )}
+              gotoPage={this.gotoPage}
+              currentPage={this.state.CURRENT_PAGE}
+            />
+          </div>
+        </div>
       </main>
     );
   };
